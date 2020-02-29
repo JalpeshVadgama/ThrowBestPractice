f7df0989310071700181bed8e602e1c88c453726
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2020-02-29 11:05:40 +0530

    Throw best practices console app

---
+++ ThrowBestPractice/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ThrowBestPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                DeviceByZero();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+        }
+
+        static int DeviceByZero()
+        {
+            try
+            {
+                int i = 10;
+                int j = 0;
+                return i / j;
+            }
+            catch (Exception)
+            {
+                // write code handle the exception
+
+                //Never use throw(ex as it will hide your actual exception)
+                throw;
+            }
+        }
+    }
+}
